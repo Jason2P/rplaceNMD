1db21190aa57f43a077b6e25a1becdee6196ec9a
Jason2P <99847264+Jason2P@users.noreply.github.com>
2022-04-04 16:11:13 -0400

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         USA Flag Collab
+// @name         Nomad Flag Collab
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  NOMAD Outline!
